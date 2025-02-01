2fe1c635b181b64697a9b47ab7321514946958ea
Kevin Ho <kho55@myseneca.ca>
2025-02-01 16:10:24 -0500

    Lab 3 complete

---
+++ lab3e.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+#Code by Kevin Ho
+
+# Create the global list called "my_list"
+my_list = [100, 200, 300, 'six hundred']
+
+def give_list():
+    # Returns all items unchanged
+    return my_list
+
+def give_first_item():
+    # Returns the first item as a string
+    return str(my_list[0])
+
+def give_first_and_last_item():
+    # Returns a list that includes the first and last items 
+    return [my_list[0], my_list[-1]]
+
+def give_second_and_third_item():
+    # Returns a list that includes the second and third items 
+    return [my_list[1], my_list[2]]
+
+if __name__ == '__main__':   # This section is the "main block"
+    print(give_list())               
+    print(give_first_item())           
+    print(give_first_and_last_item())   
+    print(give_second_and_third_item()) 
